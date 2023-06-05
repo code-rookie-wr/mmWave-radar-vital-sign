5d3e466122cf12b2e489ba9398c0df394076d585
wr <23447457@qq.com>
2023-06-05 21:06:18 +0800

    cfar函数添加了参数说明

--- vital_sign/so_cfar.m
+++ vital_sign/so_cfar.m
@@ -1,4 +1,4 @@
-function [ index, xt ] = ca_cfar(xc, N, pro_N, PAD)
+function [index, xt] = so_cfar(xc, N, pro_N, PAD)
 
 % N表示参考窗口的长度
 % pro_N表示用于检测门限的百分比
@@ -6,12 +6,12 @@ function [ index, xt ] = ca_cfar(xc, N, pro_N, PAD)
 
 alpha=N.*(PAD.^(-1./N)-1);
 index=1+N/2+pro_N/2:length(xc)-N/2-pro_N/2;
-xt=zeros(1,length(index));
+XT=zeros(1,length(index));
 
 for i=index
     cell_left=xc(1,i-N/2-pro_N/2:i-pro_N/2-1);
     cell_right=xc(1,i+pro_N/2+1:i+N/2+pro_N/2);
-    Z=(sum(cell_left)+sum(cell_right))./N;
+    Z=min([mean(cell_left),mean(cell_right)]);
 
     xt(1,i-N/2-pro_N/2)=Z.*alpha;
 end
